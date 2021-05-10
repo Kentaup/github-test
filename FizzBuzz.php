8b7a50f07ca06c75d9e1607b7086e289b68db40f
Kentaup <53824273+Kentaup@users.noreply.github.com>
2021-05-10 14:11:16 +0900

    Update FizzBuzz.php

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -7,15 +7,12 @@ for($i=1;$i<=100;$i++){
 function FizzBuzz($i){
     if($i % 15 == 0){
         print "FizzBuzz".PHP_EOL;
-        return 0;
     }elseif($i % 5 == 0){
         print "Buzz".PHP_EOL;
-        return 0;
     }elseif($i % 3 == 0){
         print "Fizz".PHP_EOL;
-        return 0;
     }else{
         print $i.PHP_EOL;
-        return 0;
     }
+    return 0;
 }
